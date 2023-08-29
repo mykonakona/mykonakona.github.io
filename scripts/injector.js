9c84465e933b582252c8b0cc0eae6f1f18e2dfdd
mykonakona <mykonakona@gmail.com>
2023-08-29 10:11:47 +0800

    add umami

---
+++ scripts/injector.js
@@ -0,0 +1,2 @@
+// <博客项目根目录>/scripts/injector.js
+hexo.extend.injector.register('head_end', '<script async defer data-website-id="1e51392e-4965-423e-837a-97b074478be2" src="https://umani-six.vercel.app/script.js"></script>');
